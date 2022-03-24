722351ed4ad6f3577119114eef58fea803b4ebd0
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-03-24 10:26:24 +0100

    Create html and js

---
+++ main.js
@@ -0,0 +1,20 @@
+let computerScore = 0
+let playerScore = 0
+
+const symbolArray = ['rock','paper','scissor'];
+
+function computerPlay() {
+    return randomSymbol = symbolArray[Math.floor(Math.random()*symbolArray.length)]
+}   
+
+console.log(computerPlay())
+
+let playerPrompt= prompt('Rock, Paper, Scissors, Shoot!').toLowerCase();
+
+function playRound(playerSelection,computerSelection){
+
+}
+
+
+console.log(playerPrompt)
+
