f037c6db5b1d997f98467f2c92f5d3a116503edf
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-03-25 10:23:42 +0100

    Add computer, player, and compare function Create random RPS function for computer and prompt for player Create compare function that returns alert for winner and loser

--- main.js
+++ main.js
@@ -4,17 +4,44 @@ let playerScore = 0
 const symbolArray = ['rock','paper','scissor'];
 
 function computerPlay() {
-    return randomSymbol = symbolArray[Math.floor(Math.random()*symbolArray.length)]
+    return computerSymbol = symbolArray[Math.floor(Math.random()*symbolArray.length)]
 }   
 
 console.log(computerPlay())
 
 let playerPrompt= prompt('Rock, Paper, Scissors, Shoot!').toLowerCase();
 
-function playRound(playerSelection,computerSelection){
+console.log(playerPrompt)
+
+function gameRound (playerDraw, computerDraw) {
+    if (playerPrompt == computerSymbol) {
+        alert('Great minds think alike!');
+        alert('Tie!');
+    } else if (playerPrompt == 'rock' && computerSymbol == 'scissor') {
+        alert('Rock beats Scissor!');
+        alert('You Win!');
+    } else if (playerPrompt == 'rock' && computerSymbol == 'paper') {
+        alert('Paper beats Rock!');
+        alert('You Lose!');
+    } else if (playerPrompt == 'scissor' && computerSymbol == 'paper') {
+        alert('Scissor beats Paper!');
+        alert('You Win!');
+    } else if (playerPrompt == 'scissor' && computerSymbol == 'rock') {
+        alert('Rock beats Scissor!');
+        alert('You Lose!');
+    } else if (playerPrompt == 'paper' && computerSymbol == 'rock') {
+        alert('Paper beats Rock!');
+        alert('You Win!');
+    } else if (playerPrompt == 'paper' && computerSymbol == 'scissor') {
+        alert('Scissor beats Paper!');
+        alert('You Lose!');
+    } else {
+        alert('Try Again!');
+    }
+}
+
+function game(gameRound) {
 
 }
 
-
-console.log(playerPrompt)
-
+console.log(gameRound())
