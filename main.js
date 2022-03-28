09cf0983a0f2838dbd5014086e996d0353a57fbe
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-03-28 15:27:35 +0200

    Add RPS logic, comperators and score system

--- main.js
+++ main.js
@@ -5,43 +5,54 @@ const symbolArray = ['rock','paper','scissor'];
 
 function computerPlay() {
     return computerSymbol = symbolArray[Math.floor(Math.random()*symbolArray.length)]
-}   
+} 
 
-console.log(computerPlay())
-
-let playerPrompt= prompt('Rock, Paper, Scissors, Shoot!').toLowerCase();
-
-console.log(playerPrompt)
-
-function gameRound (playerDraw, computerDraw) {
+function gameRound(playerPrompt, computerSymbol) {
+    playerPrompt = prompt('Rock, Paper, Scissors, Shoot!').toLowerCase();
+    computerSymbol = computerPlay()
     if (playerPrompt == computerSymbol) {
         alert('Great minds think alike!');
         alert('Tie!');
     } else if (playerPrompt == 'rock' && computerSymbol == 'scissor') {
+        playerScore += 1
         alert('Rock beats Scissor!');
-        alert('You Win!');
     } else if (playerPrompt == 'rock' && computerSymbol == 'paper') {
+        computerScore += 1
         alert('Paper beats Rock!');
-        alert('You Lose!');
     } else if (playerPrompt == 'scissor' && computerSymbol == 'paper') {
+        playerScore += 1
         alert('Scissor beats Paper!');
-        alert('You Win!');
     } else if (playerPrompt == 'scissor' && computerSymbol == 'rock') {
+        computerScore += 1
         alert('Rock beats Scissor!');
-        alert('You Lose!');
     } else if (playerPrompt == 'paper' && computerSymbol == 'rock') {
+        playerScore += 1
         alert('Paper beats Rock!');
-        alert('You Win!');
     } else if (playerPrompt == 'paper' && computerSymbol == 'scissor') {
+        computerScore += 1
         alert('Scissor beats Paper!');
-        alert('You Lose!');
     } else {
         alert('Try Again!');
+    }
+    console.log(computerSymbol)
+    alert('player: ' +playerScore);
+    alert('computer: ' +computerScore);
+    if (playerScore == 3) {
+        alert('You are the winner!')
+    } else if (computerScore == 3) {
+        alert('You are the loser!')
     }
 }
 
 function game(gameRound) {
-
+    while (playerScore < 3 && computerScore < 3) {
+    gameRound()
+    }
 }
 
-console.log(gameRound())
+
+if (playerScore == 3) {
+    alert('You are the winner!')
+} else if (computerScore == 3) {
+    alert('You are the loser!')
+}
