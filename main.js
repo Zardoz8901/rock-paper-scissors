2ca6d944e710842171ea18533c57f5d0d3410e7e
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-04-28 14:14:31 +0200

    rejink the game logic, suffer learning callbacks

--- main.js
+++ main.js
@@ -2,21 +2,33 @@
 let computerScore = 0
 let playerScore = 0
 
+// result array -1: player loss, 0: tie, 1: player victory
 const resultArray = [-1,0,1]
 
-// player selection routed to the randomized result array
+// return all html buttons as a nodelist
 const buttons = document.querySelectorAll('button');
+
+// pass the button clicks to the gameLogic funtion
 buttons.forEach((button) => {
-    button.addEventListener('click',() => {
-        playerClick = resultArray[Math.floor(resultArray.length * Math.random())];
-        playerResult =  (button.id) + " " + playerClick
-        console.log(playerResult)
+button.addEventListener('click', () => {
+    gameLogic(button);
     });
 });
 
- /* function gameResults(playerResult) {
-    if (playerResult == 1) {
-    let    document.createElement('div')
+// give each button click a random number from the resultArray
+// strip the name from the button
+// concatenate the results
+function gameLogic(button) {
+    playerClick = resultArray[Math.floor(resultArray.length * Math.random())];
+    buttonName = (button.name)
+    playerResult =  buttonName + " " + playerClick
+    console.log(playerResult)
+}
 
+// passsing function through the even is a big don don 
+function div (playerClick) {
+    if (playerClick == 0) {
+    let div = document.createElement('div')
+    document.body.appendChild(div)
     }
-}  */
+}
